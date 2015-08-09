9ff9e9e5e73ef73a3d59bfdc15d0361fe2faaa6e
Kim Kern <kim.kern@fu-berlin.de>
2015-08-09 04:36:50 +0200

    add: save config to localstorage

--- js/script.js
+++ js/script.js
@@ -5,26 +5,51 @@
 
 var model = {
     init: function () {
-        this.catlist = {
-            Maria:      {name: "Maria",       file: "second_cat.jpg",                    clicks: 0},
-            Lucy:       {name: "Lucy",        file: "1280px-Neugierige-Katze.JPG",       clicks: 0},
-            Kedi:       {name: "Kedi",        file: "Kedi-dili.jpg",                     clicks: 0},
-            Gato:       {name: "Gato",        file: "Gato_común_latinoamericano.JPG",    clicks: 0},
-            Gingeria:   {name: "Gingeria",    file: "Ginger_Kitten_Face.JPG",            clicks: 0}
-        };
-        this.currentCat = Object.keys(this.catlist).pop();
+        if (!localStorage["catlist"]) {
+            this.catlist = [
+                {name: "Maria",         file: "second_cat.jpg",                    clicks: 0},
+                {name: "Lucy",          file: "1280px-Neugierige-Katze.JPG",       clicks: 0},
+                {name: "Kedi",          file: "Kedi-dili.jpg",                     clicks: 0},
+                {name: "Gato",          file: "Gato_común_latinoamericano.JPG",    clicks: 0},
+                {name: "Marcy",         file: "laptop_cat.jpg",                    clicks: 0},
+                {name: "Pluto",         file: "screaming_kitten.jpg",              clicks: 0},
+                {name: "Paul and Lisa", file: "twocats.jpg",                       clicks: 0},
+                {name: "Gingeria",      file: "Ginger_Kitten_Face.JPG",            clicks: 0}
+            ];
+        } else {
+            this.catlist = JSON.parse(localStorage.getItem("catlist"));
+        }
+        this.currentCat = 0;
+        this.adminMode = false;
     },
-    setCurrentCatData: function (catname) {
-        this.currentCat = catname;
+    setCurrentCatData: function (catnumber) {
+        this.currentCat = catnumber;
     },
     getCurrentCatData: function () {
         return this.catlist[this.currentCat];
     },
     incrementCatCounter : function () {
         this.catlist[this.currentCat].clicks+=1;
+        model.saveCatsToLocalStorage();
     },
     getCatList: function () {
-        return Object.keys(this.catlist);
+        return this.catlist;
+    },
+    enableAdminMode: function () {
+        this.adminMode = true;
+    },
+    disableAdminMode: function() {
+        this.adminMode = false;
+    },
+    setCurrentCatProperties: function(name, url, clicks) {
+        var currentCat = this.catlist[this.currentCat];
+           currentCat["name"]=name;
+           currentCat["file"]=url;
+           currentCat["clicks"]=clicks;
+        model.saveCatsToLocalStorage();
+    },
+    saveCatsToLocalStorage: function () {
+        localStorage.setItem("catlist", JSON.stringify(this.catlist));
     }
 };
 
@@ -33,6 +58,7 @@ var controller = {
         model.init();
         catdetailview.init();
         catlistview.init();
+        adminview.init();
     },
 
     getCurrentCat: function () {
@@ -55,8 +81,39 @@ var controller = {
             model.setCurrentCatData(catName);
             catdetailview.render();
         })
+    },
+
+    addAdminButtonClickListener: function (adminButton) {
+        adminButton.click(function () {
+            model.enableAdminMode();
+            adminview.render();
+        });
+    },
+
+    addCancelButtonClickListener: function (cancelButton) {
+        cancelButton.click(function () {
+            model.disableAdminMode();
+            adminview.render();
+        });
+    },
+
+    addSaveButtonClickListener: function (saveButton) {
+        saveButton.click( function () {
+            var catName = adminview.catNameInput.prop("value");
+            var catURL = adminview.catURLInput.prop("value");
+            var catClicks = parseInt(adminview.catClickInput.prop("value"));
+            model.setCurrentCatProperties(catName, catURL, catClicks);
+            catdetailview.render();
+            model.disableAdminMode();
+            adminview.render();
+            catlistview.render();
+        });
+    },
+
+    getAdminMode: function () {
+        return model.adminMode;
     }
-};
+ };
 
 var catdetailview = {
     init: function () {
@@ -64,14 +121,17 @@ var catdetailview = {
         var catHTML = "<p>Catclicks for <span id='cat_name'></span>: <span id='cat_clicks'></span></p>" +
             "<img id='cat_image' src='' width='400'>";
         this.catdisplay.append(catHTML);
-        controller.addCatImageClickListener($("#cat_image"));
+        this.catName = $("#cat_name");
+        this.catClicks = $("#cat_clicks");
+        this.catImage = $("#cat_image");
+        controller.addCatImageClickListener(this.catImage);
         catdetailview.render();
     },
     render: function () {
         var currentCat = controller.getCurrentCat();
-        $("#cat_name").text(currentCat.name);
-        $("#cat_clicks").text(currentCat.clicks);
-        $("#cat_image").attr("src", 'img/' + currentCat.file);
+        this.catName.text(currentCat.name);
+        this.catClicks.text(currentCat.clicks);
+        this.catImage.attr("src", 'img/' + currentCat.file);
     }
 };
 
@@ -80,14 +140,48 @@ var catlistview = {
         var cats = controller.getCatList();
         var catHTML = [];
         for (c in cats) {
-            var cat = cats[c];
-            var newButton = $("<button>" + cat + "</button>");
-            controller.addCatButtonClickListener(newButton, cat);
+            var catName = cats[c].name;
+            var newButton = $("<button id='" + c + "'></button>");
+            controller.addCatButtonClickListener(newButton, c);
             catHTML.push(newButton);
         }
         $("#cat_listview").append(catHTML);
+        catlistview.render();
+    },
+    render: function () {
+        var cats = controller.getCatList();
+        for (c in cats) {
+            var button = $("#" + c);
+            button.text(cats[c].name);
+        }
     }
 };
 
+var adminview = {
+    init: function () {
+        this.adminButton = $("#admin_button");
+        this.adminSection = $("#admin_section");
+        this.cancelButton = $("#admin_cancel");
+        this.saveButton = $("#admin_save");
+        this.catNameInput = $("#admin_name");
+        this.catURLInput = $("#admin_url");
+        this.catClickInput = $("#admin_clicks");
+        controller.addAdminButtonClickListener(this.adminButton);
+        controller.addCancelButtonClickListener(this.cancelButton);
+        controller.addSaveButtonClickListener(this.saveButton);
+        adminview.render();
+    },
+    render: function () {
+        if (controller.getAdminMode()) {
+            this.adminSection.attr("style", "display:inline");
+            var currentCat = controller.getCurrentCat();
+            this.catNameInput.prop("value", currentCat.name);
+            this.catURLInput.prop("value", currentCat.file);
+            this.catClickInput.prop("value", currentCat.clicks);
+        } else {
+            $(this.adminSection).attr("style", "display:none")
+        }
+    }
+};
 
 controller.init();
